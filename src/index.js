cb69ed8dd2b56086708ef29576647a88b32cfa9c
jelenapiha <133747173+jelenapiha@users.noreply.github.com>
2023-12-06 21:37:05 +0100

    Edited time

--- src/index.js
+++ src/index.js
@@ -31,6 +31,13 @@ function formatDate(date) {
   ];
   let day = days[date.getDay()];
 
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
   return `${day} ${hours}:${minutes}`;
 }
 
