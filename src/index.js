b139fe00452ace2f2bf7d780c277a600c28035e0
jelenapiha <133747173+jelenapiha@users.noreply.github.com>
2023-12-15 14:07:45 +0100

    Injected forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -59,5 +59,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature">
+        <strong>15°</strong>
+      </span>
+      <span class="weather-forecast-temperature">10°</div>
+    </span>
+  </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+displayForecast();
